9fd36967ea1910f901a37890fdcbc7a07c04168e
rowmendza <rowmendz@mail.sfsu.edu>
2015-10-26 06:05:31 +0000

    initial commit

--- async_io.js
+++ async_io.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
-var filename = process.argv[2];
-file = fs.readFile(filename, function(err, data) {
-  console.log(data.toString().split('\n').length - 1);
+var path = process.argv[2];
+
+fs.readFile(path, 'utf8', function(err,data) {
+  var lines = data.split('\n');
+  console.log(lines.length-1);
 });
---
+++ modules/module.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+
+module.exports = function(dirName, extName, callback){
+  fs.readdir(dirName, function(err, data){
+    if (err){
+      return callback(err);
+    }
+    var regexText = new RegExp('.+ \.' + extName + '$');
+    data.filter(function(elem){
+      return regexText.test(elem);
+    });
+    
+    callback(null, data);
+    
+    
+  });
+  
+}
